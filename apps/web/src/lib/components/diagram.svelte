fae58c9ae0b99bf9a4bc4419197128b7f7f6c753
Bruno de Araujo Alves <me@baraus.dev>
2024-08-30 08:11:46 -0300

    feat: change header colors

--- apps/web/src/lib/components/diagram.svelte
+++ apps/web/src/lib/components/diagram.svelte
@@ -8,61 +8,57 @@
 
 	let { tree }: Props = $props();
 
+	function extractEntities(node: SyntaxNode | undefined): Entity[] {
+		if (!node) {
+			return [];
+		}
 
-    function extractEntities(node: SyntaxNode | undefined): Entity[] {
-        if (!node) {
-            return [];
-        }
+		const entities: Entity[] = [];
 
-        const entities: Entity[] = [];
+		node.children.forEach((child, index) => {
+			if (['aggregate', 'entity', 'value_object', 'enum'].includes(child.type)) {
+				const entity: Entity = {
+					id: child.child(1)?.text.replaceAll('<', '').replaceAll('>', ''),
+					name: ['aggregate', 'entity'].includes(child.type)
+						? child.child(2)?.text
+						: child.child(1)?.text,
+					type: child.type,
+					values: [],
+					properties: [],
+					position: {
+						x: 0,
+						y: 0,
+						width: 0,
+						height: 0
+					}
+				};
 
-        node.children.forEach((child, index) => {
-            if (['aggregate', 'entity', 'value_object', 'enum'].includes(child.type)) {
-                const entity: Entity = {
-                    id: child.child(1)?.text.replaceAll("<", "").replaceAll(">", ""),
-                    name: ['aggregate', 'entity'].includes(child.type) ? child.child(2)?.text : child.child(1)?.text,
-                    values: [],
-                    properties: [],
-                    position: {
-                        x: 0,
-                        y: 0,
-                        width: 0,
-                        height: 0
-                    }
-                };
+				child.children.forEach((grandchild) => {
+					if (grandchild.type === 'enum_value') {
+						entity.values.push(grandchild.child(0)?.text);
+					}
 
-                child.children.forEach(grandchild => {
-                    if (grandchild.type === 'enum_value') {
-                        entity.values.push(
-                            grandchild.child(0)?.text,
-                        );
-                    }
+					if (grandchild.type === 'field') {
+						entity.properties.push({
+							name: grandchild.child(0)?.text,
+							type: grandchild.child(2)?.text
+						});
+					}
 
-                    if (grandchild.type === 'field') {
-                        entity.properties.push({
-                            name: grandchild.child(0)?.text,
-                            type: grandchild.child(2)?.text
-                        });
-                        
-                    }
+					entity.position = {
+						x: index * 200,
+						y: 100,
+						width: 200,
+						height: 100
+					};
+				});
 
-                    entity.position = {
-                            x: index * 200,
-                            y: 100,
-                            width: 200,
-                            height: 100
-                        };
-                });
+				entities.push(entity);
+			}
+		});
 
-
-                entities.push(entity);
-            }
-
-        });
-
-        return entities;
-    }
-
+		return entities;
+	}
 </script>
 
 <div width="800" height="600" use:diagram={extractEntities(tree?.rootNode)}></div>
