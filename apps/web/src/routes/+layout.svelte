0c36a6a3f4bbfd9dedd1a9f52fc07b622cdb4b04
Bruno de Araujo Alves <me@baraus.dev>
2024-09-15 15:38:47 -0300

    refactor: only add plausible on production

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { browser } from '$app/environment';
 	import { queryClient } from '$lib/client';
+	import { NODE_ENV } from '$env/static/private';
 	import '../app.css';
 
 	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
@@ -18,6 +19,13 @@
 
 <svelte:head>
 	<title>Domain Driven Diagram</title>
+	{#if NODE_ENV === 'production'}
+		<script
+			defer
+			data-domain="dddiagram.baraus.dev"
+			src="https://plausible.lab.baraus.dev/js/script.js"
+		></script>
+	{/if}
 </svelte:head>
 
 <QueryClientProvider client={$queryClient}>
