d8c8ef0d5376bcdfc50f3d20dbbb6d35bc4978d0
Bruno de Araujo Alves <me@baraus.dev>
2024-09-21 01:29:00 -0300

    feaT: add cursors

--- apps/web/src/lib/components/editor.svelte
+++ apps/web/src/lib/components/editor.svelte
@@ -1,16 +1,53 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { PUBLIC_WS_URL } from '$env/static/public';
+	import { getAvatarColor } from '$lib';
 	import { editor, model, monaco } from '$lib/store';
+	import { color } from 'd3';
 	import _ from 'lodash';
-	import * as Y from 'yjs';
 	import { onDestroy, onMount } from 'svelte';
+	import * as Y from 'yjs';
+
+	const backgrounds = [
+		'bg-red-500',
+		'bg-blue-500',
+		'bg-green-500',
+		'bg-yellow-500',
+		'bg-indigo-500',
+		'bg-purple-500',
+		'bg-pink-500',
+		'bg-gray-500'
+	];
+
+	const borders = [
+		'border-l-red-500',
+		'border-l-blue-500',
+		'border-l-green-500',
+		'border-l-yellow-500',
+		'border-l-indigo-500',
+		'border-l-purple-500',
+		'border-l-pink-500',
+		'border-l-gray-500'
+	];
 
 	let el: HTMLDivElement;
 	let provider;
 	let type;
 	let doc;
 	let binding;
+	let awareness;
+	let decorations;
+
+	interface CursorsType {
+		lineNumber: number;
+		column: number;
+	}
+	interface SelectionsType {
+		startLineNumber: number;
+		startColumn: number;
+		endLineNumber: number;
+		endColumn: number;
+	}
 
 	let props: {
 		value: string;
@@ -52,22 +89,95 @@
 		type = doc.getText('monaco');
 		binding = new MonacoBinding(type, $editor?.getModel()!, new Set([$editor]), provider.awareness);
 
-		provider.awareness.setLocalStateField('user', {
-			name: $page.data.user.name,
-			color: '#f3c2d8'
+		awareness = provider.awareness;
+		awareness.setLocalState({
+			user: {
+				id: $page.data.user.id,
+				name: $page.data.user.name
+			}
 		});
 
-		// provider.awareness.on('change', () => {
-		// 	// Map each awareness state to a dom-string
-		// 	const strings = [];
-		// 	provider.awareness.getStates().forEach((state) => {
-		// 		console.log(state);
-		// 		if (state.user) {
-		// 			strings.push(`<div style="color:${state.user.color};">• ${state.user.name}</div>`);
-		// 		}
-		// 		document.querySelector('#users').innerHTML = strings.join('');
-		// 	});
-		// });
+		// Update cursor positions for each user
+		$editor.onDidChangeCursorPosition((e: any) => {
+			decorations?.clear();
+			const cursor = e.position;
+			const localState = awareness?.getLocalState();
+			awareness?.setLocalState({
+				...localState,
+				cursor
+			});
+		});
+
+		$editor.onDidChangeCursorSelection((e: any) => {
+			decorations?.clear();
+			const selection = e.selection;
+			let localState = awareness?.getLocalState();
+			awareness?.setLocalState({
+				...localState,
+				selection
+			});
+		});
+
+		awareness.on('change', () => {
+			decorations?.clear();
+
+			decorateCursors();
+		});
+
+		function decorateCursors() {
+			const r: any = [];
+
+			for (const deco of Array.from(
+				awareness
+					.getStates()
+					.values()
+					.filter((s) => s.user.id !== $page.data.user.id)
+			)) {
+				const user = deco.user;
+				const cursor = deco.cursor;
+				const selection = deco.selection;
+				console.log('cursor', user);
+
+				if (cursor) {
+					r.push({
+						range: new $monaco.Range(
+							cursor.lineNumber,
+							cursor.column,
+							cursor.lineNumber,
+							cursor.column + 1
+						),
+						options: {
+							className: `${borders[user.id % borders.length]} opacity-50 rounded border-l-solid border-l-4 animate-pulse`,
+							hoverMessage: {
+								value: user.name,
+								isTrusted: true
+							}
+						}
+					});
+				}
+
+				if (selection) {
+					r.push({
+						range: new $monaco.Range(
+							selection.startLineNumber,
+							selection.startColumn,
+							selection.endLineNumber,
+							selection.endColumn
+						),
+						options: {
+							className: `${backgrounds[user.id % backgrounds.length]} opacity-50 rounded`,
+							hoverMessage: {
+								value: user.name,
+								isTrusted: true
+							}
+						}
+					});
+				}
+			}
+
+			decorations?.clear();
+			decorations = $editor?.createDecorationsCollection(r);
+		}
 
 		$model.onDidChangeContent((e) => {
 			props.onchange?.($model.getValue());
@@ -94,14 +204,6 @@
 	onDestroy(() => {
 		dispose();
 	});
-
-	$effect(() => {
-		if (!$model || $model?.isDisposed() || _.isEqual($model?.getValue(), props.value)) {
-			return;
-		}
-
-		$model?.setValue(props.value);
-	});
 </script>
 
 <div
@@ -119,4 +221,6 @@
 	#editor {
 		width: calc((100vw * 0.3) - 48px);
 	}
+
+	/* Add color per user */
 </style>
