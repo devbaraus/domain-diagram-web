8aa20e654c20346ae5c891bb57e90a42c88b8ab4
Bruno de Araújo Alves <me@baraus.dev>
2024-09-30 11:57:55 -0300

    feat: embedable diagram

--- apps/api/src/websocket.ts
+++ apps/api/src/websocket.ts
@@ -113,49 +113,6 @@ export const startWebSocketServer = (server: any) => {
             yjs.docs.set(docName, ydoc);
         }
 
-        console.log('Yjs document created:', docName);
-
         yjs.setupWSConnection(ws, req, ydoc);
     })
 };
-
-
-
-function logDocContent(doc: Y.Doc) {
-    console.log('Logging Y.Doc content:');
-
-    // Iterate over all shared types in the document
-    doc.share.forEach((type, key) => {
-        console.log(`Type: ${type.constructor.name}, Key: ${key}`);
-
-        if (type instanceof Y.Map) {
-            logMapContent(type);
-        } else if (type instanceof Y.Array) {
-            logArrayContent(type);
-        } else if (type instanceof Y.Text) {
-            logTextContent(type);
-        }
-    });
-}
-
-// Helper function to log Y.Map content
-function logMapContent(map: Y.Map<any>) {
-    console.log('Y.Map content:');
-    map.forEach((value, key) => {
-        console.log(`  Key: ${key}, Value: ${value}`);
-    });
-}
-
-// Helper function to log Y.Array content
-function logArrayContent(arr: Y.Array<any>) {
-    console.log('Y.Array content:');
-    arr.toArray().forEach((item, index) => {
-        console.log(`  Index: ${index}, Item: ${item}`);
-    });
-}
-
-// Helper function to log Y.Text content
-function logTextContent(text: Y.Text) {
-    console.log('Y.Text content:');
-    console.log(`  Text: ${text.toString()}`);
-}
--- apps/web/src/routes/+layout.server.ts
+++ apps/web/src/routes/+layout.server.ts
@@ -6,9 +6,6 @@ const pubMatcher = /^\/(docs|embeds\/\d+)/
 export const load: LayoutServerLoad = async ({ locals, url, parent }) => {
     await parent()
 
-    console.log('layout load', url.pathname, pubMatcher.test(url.pathname))
-
-
     if (pubMatcher.test(url.pathname)) {
         return {}
     }
