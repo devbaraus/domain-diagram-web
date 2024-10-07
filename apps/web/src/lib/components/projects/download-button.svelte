abb2e107902d9e8580777967d01ecec67e86cbae
Bruno de Araújo Alves <me@baraus.dev>
2024-10-07 08:10:25 -0300

    feat: parameterize image download

--- apps/web/src/lib/components/projects/download-button.svelte
+++ apps/web/src/lib/components/projects/download-button.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { DownloadIcon } from 'lucide-svelte';
+	import { DownloadIcon, ChevronDownIcon } from 'lucide-svelte';
 	import { diagram } from '$lib/store';
 
 	let processing = false;
+	let modal: HTMLDialogElement;
+	let widthPerContext = 1200;
+	let imageformat = 'image/png';
 
 	function downloadPng() {
 		processing = true;
@@ -24,21 +27,26 @@
 			`-40 -40 ${group?.getBoundingClientRect().width} ${group?.getBoundingClientRect().height}`
 		);
 
-		svgToCanvasAndDownload(svg, 'diagram.png', () => {
+		const contexts = Math.ceil(Math.sqrt($diagram.contexts.length));
+		const contextWidth = contexts * widthPerContext;
+		const scale = contextWidth / svg?.getBBox().width;
+
+		svgToCanvasAndDownload(svg, 'diagram.png', scale, () => {
 			group?.setAttribute('transform', transform);
 			svg?.setAttribute('width', width);
 			svg?.setAttribute('height', height);
 			svg?.setAttribute('viewBox', viewBox);
 			processing = false;
+			modal.close();
 		});
 	}
 
-	function svgToCanvasAndDownload(svgElement, filename = 'image.png', callback: Function) {
-		const contexts = Math.ceil(Math.sqrt($diagram.contexts.length));
-		const widthPerContext = 1200;
-		const width = contexts * widthPerContext;
-		const scale = width / svgElement.getBBox().width;
-
+	function svgToCanvasAndDownload(
+		svgElement,
+		filename = 'image.png',
+		scale: number,
+		callback: Function
+	) {
 		const svgData = new XMLSerializer().serializeToString(svgElement); // Convert SVG to XML string
 		const svgBlob = new Blob([svgData], { type: 'image/svg+xml;charset=utf-8' }); // Create a Blob from the SVG
 		const url = URL.createObjectURL(svgBlob); // Create a URL for the Blob
@@ -57,8 +65,10 @@
 
 			// Create the download link
 			const a = document.createElement('a');
-			a.href = canvas.toDataURL('image/png');
-			a.download = $page.data.item?.name ? `${$page.data.item.name}.png` : filename;
+			a.href = canvas.toDataURL(imageformat);
+
+			const ext = imageformat.split('/')[1];
+			a.download = $page.data.item?.name ? `${$page.data.item.name}.${ext}` : filename;
 			document.body.appendChild(a);
 			a.click();
 
@@ -73,8 +83,42 @@
 </script>
 
 <div class="tooltip tooltip-right" data-tip="Download">
-	<button disabled={processing} class="btn btn-info mask mask-squircle" onclick={downloadPng}>
+	<button
+		disabled={processing}
+		class="btn btn-info mask mask-squircle"
+		onclick={() => modal.showModal()}
+	>
 		<span class="sr-only">download</span>
 		<DownloadIcon class="size-5" />
 	</button>
 </div>
+
+<dialog bind:this={modal} class="modal">
+	<div class="modal-box">
+		<h3 class="mb-2 text-lg font-bold">Configure download</h3>
+		<label class="input input-bordered flex items-center gap-2">
+			Context Width (px)
+			<input inputmode="numeric" bind:value={widthPerContext} />
+		</label>
+		<label class="input input-bordered flex items-center gap-2">
+			Image Format
+			<select bind:value={imageformat}>
+				<option value="image/png">PNG</option>
+				<option value="image/jpeg">JPEG</option>
+				<option value="image/webp">WebP</option>
+			</select>
+		</label>
+		<div class="modal-action">
+			<form method="dialog">
+				<!-- if there is a button in form, it will close the modal -->
+				<button class="btn">Close</button>
+				<button class="btn btn-primary" disabled={processing} onclick={downloadPng}>Download</button
+				>
+			</form>
+		</div>
+	</div>
+
+	<form method="dialog" class="modal-backdrop">
+		<button>close</button>
+	</form>
+</dialog>
