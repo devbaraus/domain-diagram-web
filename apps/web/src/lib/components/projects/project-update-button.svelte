a9d4fc57b18d3ef761d0e3d1f7d36ae41bf4a8ae
Bruno de Araújo Alves <b.araujo@kingspanisoeste.com.br>
2024-10-09 13:35:43 -0300

    refactor: improve update modal

--- apps/web/src/lib/components/projects/project-update-button.svelte
+++ apps/web/src/lib/components/projects/project-update-button.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { queryClient } from '$lib/client';
 	import { updateProject } from '$lib/services/project-service.svelte';
@@ -11,6 +10,9 @@
 	let modal: HTMLDialogElement | undefined;
 
 	let members = $state([]);
+	let name = $state($page.data.item.name);
+	let pub = $state($page.data.item.public);
+	let realtime = $state($page.data.item.realtime);
 
 	$effect(() => {
 		members = $page.data.item?.members.map((i) => ({ email: i.user.email, role: i.role }));
@@ -29,99 +31,117 @@
 			toast.error('Failed to share project');
 		}
 	});
+
+	function handleSubmit() {
+		$update.mutate({
+			name: name,
+			public: pub,
+			realtime: realtime,
+			members: members
+		});
+	}
 </script>
 
 <dialog bind:this={modal} class="modal">
 	<div class="modal-box space-y-2">
 		<p>Update the settings this project</p>
 
-		<form
-			onsubmit={(e) => {
-				e.preventDefault();
-				const formData = new FormData(e.target);
-				const name = formData.get('name') as string;
-				const pub = formData.get('public') === 'on';
-				const newMembers = members.map((i) => ({ email: i.email, role: i.role }));
+		<label class="label cursor-pointer justify-start gap-2">
+			<span class="label-text">Name</span>
+			<input
+				class="input input-sm input-bordered flex w-full items-center gap-2"
+				name="name"
+				minlength="3"
+				type="text"
+				placeholder="Daisy"
+				value={$page.data.item.name}
+			/>
+		</label>
 
-				$update.mutate({ name, public: pub, members: newMembers });
-			}}
-		>
-			<label class="label cursor-pointer justify-start gap-2">
-				<span class="label-text">Name</span>
-				<input
-					class="input input-sm input-bordered flex w-full items-center gap-2"
-					name="name"
-					minlength="3"
-					type="text"
-					placeholder="Daisy"
-					value={$page.data.item.name}
-				/>
-			</label>
+		<label class="label cursor-pointer justify-start gap-2">
+			<span class="label-text">Public</span>
+			<input name="public" type="checkbox" class="toggle" bind:checked={pub} />
+		</label>
 
-			<label class="label cursor-pointer justify-start gap-2">
-				<span class="label-text">Public</span>
-				<input name="public" type="checkbox" class="toggle" checked={$page.data.item.public} />
-			</label>
+		<label class="label cursor-pointer justify-start gap-2">
+			<span class="label-text">Realtime</span>
+			<input name="public" type="checkbox" class="toggle" bind:checked={realtime} />
+		</label>
 
-			<div class="grid border-collapse grid-cols-2 gap-2">
-				{#if members}
-					{#each members as member, index}
+		<div class="space-y-1">
+			{#if members}
+				{#each members as member, index}
+					<div class="grid border-collapse grid-cols-5 gap-2">
 						<input
-							name="members[].email"
-							class={cn('input input-sm input-bordered', member.role === 'OWNER' && 'col-span-2')}
+							bind:value={member.email}
+							class={'input input-sm input-bordered col-span-3'}
 							type="text"
 							disabled={member.role === 'OWNER'}
-							value={member.email}
 						/>
-						<input type="text" name="members[].role" class="hidden" value={member.role} />
+
+						<select bind:value={member.role} class="col-span-1" disabled={member.role === 'OWNER'}>
+							<option value="OWNER">OWNER</option>
+							<option value="VIEWER">VIEWER</option>
+							<option value="EDITOR">EDITOR</option>
+						</select>
+
 						<button
 							type="button"
-							class={cn('btn btn-sm', member.role === 'OWNER' && 'hidden')}
+							class={cn('btn btn-sm')}
+							disabled={member.role === 'OWNER'}
 							onclick={() => (members = members.filter((i) => i.email !== member.email))}
 							>Remove
 						</button>
-					{/each}
-				{/if}
-			</div>
-			<div class="mt-2">
+					</div>
+				{/each}
+			{/if}
+		</div>
+		<div class="label mt-2">
+			<form
+				class="grid w-full grid-cols-5 gap-2"
+				onsubmit={(e) => {
+					e.preventDefault();
+
+					const email = e.target[0].value;
+					const role = e.target[1].value;
+
+					if (members.find((i) => i.email === email)) return;
+
+					members.push({ email: email, role: role });
+
+					e.target[0].value = '';
+					e.target[1].value = 'VIEWER';
+				}}
+			>
 				<input
-					class="input input-bordered input-sm w-full"
+					id="member"
+					class="input input-bordered input-sm col-span-3 w-full"
 					type="email"
-					onkeydown={(e) => {
-						if (e.key === 'Enter') {
-							e.preventDefault();
-							const email = e.currentTarget.value;
-
-							if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) === false) {
-								toast.error('Invalid email address');
-								return;
-							}
-
-							if (members.find((i) => i.email === email)) return;
-							members.push({ email: email, role: 'VIEWER' });
-
-							e.currentTarget.value = '';
-						}
-					}}
+					placeholder="Add member by email"
 				/>
-			</div>
+				<select name="role">
+					<option value="VIEWER">VIEWER</option>
+					<option value="EDITOR">EDITOR</option>
+				</select>
+				<button class={'btn btn-sm'}>Add</button>
+			</form>
+		</div>
 
-			<div class="modal-action">
-				<button
-					type="button"
-					class="btn mr-auto"
-					onclick={() => {
-						navigator.clipboard.writeText(
-							`${$page.url.protocol}//${$page.url.host}/e/${$page.params.id}?token=${$page.data.item.embed}`
-						);
-						modal?.close();
-						toast.success('Embed code copied to clipboard');
-					}}>Embed</button
-				>
-				<button type="button" class="btn" onclick={() => modal?.close()}>Cancel</button>
-				<button class="btn btn-primary">Save</button>
-			</div>
-		</form>
+		<div class="modal-action">
+			<button
+				type="button"
+				class="btn mr-auto"
+				onclick={() => {
+					navigator.clipboard.writeText(
+						`${$page.url.protocol}//${$page.url.host}/e/${$page.params.id}?token=${$page.data.item.embed}`
+					);
+					modal?.close();
+					toast.success('Embed code copied to clipboard');
+				}}>Embed</button
+			>
+			<button type="button" class="btn" onclick={() => modal?.close()}>Cancel</button>
+			<button class="btn btn-primary" onclick={handleSubmit}>Save</button>
+		</div>
 	</div>
 </dialog>
 
